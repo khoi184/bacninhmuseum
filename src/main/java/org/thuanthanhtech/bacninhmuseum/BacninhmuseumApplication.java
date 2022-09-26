02bf518f82e4f938d9def6c450c00dce67882bcc
blackneko184 <khoinq.184@gmail.com>
2022-09-26 15:32:00 +0700

    first commit

---
+++ src/main/java/org/thuanthanhtech/bacninhmuseum/BacninhmuseumApplication.java
@@ -0,0 +1,13 @@
+package org.thuanthanhtech.bacninhmuseum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacninhmuseumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BacninhmuseumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/thuanthanhtech/bacninhmuseum/BacninhmuseumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thuanthanhtech.bacninhmuseum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacninhmuseumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
