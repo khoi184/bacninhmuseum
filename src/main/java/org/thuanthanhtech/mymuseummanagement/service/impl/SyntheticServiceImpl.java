79a594600ecedbbfc74a71f4e58cf6f1fbd3592a
Admin <123456>
2022-10-10 15:20:11 +0700

    fg

--- src/main/java/org/thuanthanhtech/mymuseummanagement/service/impl/SyntheticServiceImpl.java
+++ src/main/java/org/thuanthanhtech/mymuseummanagement/service/impl/SyntheticServiceImpl.java
@@ -24,7 +24,6 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class SyntheticServiceImpl implements SyntheticService {
 
-
     private final SyntheticRepository syntheticRepository;
 
     @SneakyThrows
